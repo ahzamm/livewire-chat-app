befd78807f5381864f3ceadfeba6ef2a25f79577
ahzam <ahzamahmed6@gmail.com>
2025-05-31 21:40:20 +0500

    rewrite authenticate middleware

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/signup', \App\Livewire\Auth\Signup::class)->name('signup');
 Route::get('/forget-password', \App\Livewire\Auth\ForgetPassword::class)->name('forget-password');
 Route::get('/reset-password/{token}', \App\Livewire\Auth\ResetPassword::class)->name('password.reset');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(\App\Http\Middleware\Authenticate::class)->group(function () {
     Route::get('/', \App\Livewire\ChatWindow::class)->name('chat-window');
     Route::post('/signout', \App\Livewire\Auth\Signout::class)->name('signout');
 });
